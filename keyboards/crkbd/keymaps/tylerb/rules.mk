6c5fa40b891f5ac4b51623560b922aa46f195a92
Tyler Barnes <tylerdbarnes@gmail.com>
2024-03-15 13:07:47 -0700

    map

---
+++ keyboards/crkbd/keymaps/tylerb/rules.mk
@@ -0,0 +1,7 @@
+# MOUSEKEY_ENABLE = no     # Mouse keys
+# RGBLIGHT_ENABLE = no
+# RGB_MATRIX_ENABLE = no # Enable WS2812 RGB underlight.
+# VIA_ENABLE      = no # Enable VIA
+OLED_ENABLE     = yes
+LTO_ENABLE      = yes
+# WPM_ENABLE = yes
